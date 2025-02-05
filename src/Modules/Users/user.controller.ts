41cca52d20bf86e3507879438eec23c46dbcaa06
lucaasandrade <lucasmarcelodeandradesilva@gmail.com>
2025-02-05 10:57:49 -0300

    err: circular dependency

--- src/Modules/Users/user.controller.ts
+++ src/Modules/Users/user.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Post,
   Put,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
@@ -14,17 +15,25 @@ import { UpdatePatchUIserDTO } from './dto/update-patch-user.dto';
 import { UserService } from './user.service';
 import { LogInterceptor } from 'src/interceptors/log.interceptor';
 import { ParamId } from 'src/decorators/param-id.decorator';
+import { RoleGuard } from 'src/guards/role.guard';
+import { Roles } from 'src/decorators/role.decorator';
+import { Role } from 'src/enums/role.enum';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('users')
+@UseInterceptors(LogInterceptor)
+@UseGuards(AuthGuard, RoleGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @UseInterceptors(LogInterceptor)
   @Post()
+  @Roles(Role.Admin)
   async create(@Body() data: CreateUserDTO) {
     return this.userService.create(data);
   }
 
+  @Roles(Role.Admin)
   @Get()
   async read() {
     return this.userService.list();
